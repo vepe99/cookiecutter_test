3a5fb90129e80bcf1b37d8f3476baaac60360153
vepe99 <viterbogiuseppe99@gmail.com>
2024-03-12 16:16:41 +0100

    created first module

---
+++ cookiecutter_test/cockiecutter_module/__init__.py
@@ -0,0 +1,6 @@
+"""
+================================================
+cockiecutter module test
+================================================
+This module contain the test module for the cookiecutter_test package
+"""
