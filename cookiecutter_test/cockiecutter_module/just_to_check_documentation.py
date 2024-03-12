5a3287c873316130b524035336b5a12563245276
vepe99 <viterbogiuseppe99@gmail.com>
2024-03-12 16:22:50 +0100

    added function print_smile in module just_to_check_documentation

---
+++ cookiecutter_test/cockiecutter_module/just_to_check_documentation.py
@@ -0,0 +1,14 @@
+"""
+===================
+Just to check how does the documentaion work
+===================
+
+this module just import add_one and 
+
+"""
+
+from ...cookiecutter_test import add_one
+import numpy as np
+
+def print_smile():
+    print('smile')
