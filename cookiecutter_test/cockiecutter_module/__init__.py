6805565d0efae8af807e6f92accbf0daaf8ff360
vepe99 <viterbogiuseppe99@gmail.com>
2024-03-12 20:01:55 +0100

    tests

--- cookiecutter_test/cockiecutter_module/__init__.py
+++ cookiecutter_test/cockiecutter_module/__init__.py
@@ -4,4 +4,4 @@ cockiecutter module test
 ================================================
 This module contain the test module for the cookiecutter_test package
 """
-from . import just_to_check_documentation
+from cockiecutter_module import just_to_check_documentation
